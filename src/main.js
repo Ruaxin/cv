4dcc14abc3da693abf7f98e2d6995bc8c2098e57
ruaxin <qy.xin@foxmail.com>
2020-04-09 15:01:43 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,48 @@
+        var myChart = echarts.init(document.getElementById('skills'));
+
+        var option = {
+            tooltip: {},
+            radar: {
+                name: {
+                    textStyle: {
+                        color: '#fff',
+                        backgroundColor: '#999',
+                        borderRadius: 3,
+                        padding: [3, 5]
+                    }
+                },
+                indicator: [{
+                        name: '静态页面',
+                        max: 100
+                    },
+                    {
+                        name: '编程基础',
+                        max: 100
+                    },
+                    {
+                        name: 'Vue',
+                        max: 100
+                    },
+                    {
+                        name: 'React',
+                        max: 100
+                    },
+                    {
+                        name: '项目开发',
+                        max: 100
+                    },
+                    {
+                        name: '沟通能力',
+                        max: 100
+                    }
+                ]
+            },
+            series: [{
+                type: 'radar',
+                data: [{
+                    value: [80, 70, 65, 65, 60, 65],
+                    name: '能力雷达'
+                }]
+            }]
+        };
+        myChart.setOption(option);
